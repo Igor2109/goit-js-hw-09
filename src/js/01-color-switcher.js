0e27f630cfc3d62eaa07542bf9b4b692a15cc5df
Igor2109 <127054357+Igor2109@users.noreply.github.com>
2023-08-05 16:59:26 +0200

    timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-function name(params) {}
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+let intervalId = null;
+stopBtn.disabled = true;
+
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', e => {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  e.target.setAttribute('disabled', true);
+  e.target.nextElementSibling.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener('click', e => {
+  clearInterval(intervalId);
+  e.target.setAttribute('disabled', true);
+  e.target.previousElementSibling.removeAttribute('disabled');
+});
